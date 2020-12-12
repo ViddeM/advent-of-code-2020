bcfbc6f90bd9d1133c1bd232fd994d6bb275a3e5
Vidar Magnusson <git@vidarmagnusson.com>
2020-12-12 15:56:46 +0100

    Add day 12

--- 12/main.go
+++ 12/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -24,7 +25,19 @@ type Move struct {
 }
 
 func main() {
-	f, err := os.Open("11/input.txt")
+	moves := parseInput()
+
+	solution := solvePartOne(moves)
+	fmt.Printf("Part One -- The solution is %d\n", solution)
+
+
+	solution2 := solvePartTwo(moves)
+	fmt.Printf("Part Two -- The solution is %d\n", solution2)
+}
+
+func parseInput() []Move {
+	f, err := os.Open("input.txt")
+	//f, err := os.Open("12/test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,16 +60,27 @@ func main() {
 			steps: steps,
 		})
 	}
+	return moves
+}
 
-	solution := solvePartOne(moves)
-	fmt.Printf("Part One -- The solution is %d\n", solution)
-
-
-/*	solution2 := solvePartTwo(lay)
-	fmt.Printf("Part Two -- The solution is %d\n", solution2)*/
+func absInt(a int) int {
+	return int(math.Abs(float64(a)))
 }
 
 func forward(dir, steps int) (int, int) {
+	switch dir {
+	case 0:
+		return 0, steps
+	case 1:
+		return steps, 0
+	case 2:
+		return 0, -steps
+	case 3:
+		return -steps, 0
+	default:
+		log.Fatal(fmt.Sprintf("Invalid dir num %d", dir))
+	}
+	return 0, 0
 }
 
 func solvePartOne(moves []Move) int {
@@ -78,16 +102,83 @@ func solvePartOne(moves []Move) int {
 			x -= move.steps
 			break
 		case R:
-			dir = (dir + 1) % 4
+			dir = (dir + (move.steps / 90)) % 4
 			break
 		case L:
-			dir = (dir - 1) % 4
+			dir = (dir - (move.steps / 90)) % 4
+			if dir < 0 {
+				dir += 4
+			}
 			break
 		case F:
-			forward(dir, )
+			xInc, yInc := forward(dir, move.steps)
+			x += xInc
+			y += yInc
 			break
 		default:
 			log.Panic(fmt.Sprintf("Invalid direction dir: '%s'", move.dir))
 		}
 	}
+
+	x = absInt(x)
+	y = absInt(y)
+
+	return x + y
+}
+
+func rotateRight(amount, x , y int) (int, int) {
+	for i := 0; i < amount; i++ {
+		tmp := y
+		y = -x
+		x = tmp
+	}
+	return x, y
+}
+
+func solvePartTwo(moves []Move) int {
+	wayX := 10
+	wayY := 1
+	shipX := 0
+	shipY := 0
+	dir := 1 // 0 = N, 1 = E...
+	for _, move := range moves {
+		switch move.dir {
+		case N:
+			wayY += move.steps
+			break
+		case S:
+			wayY -= move.steps
+			break
+		case E:
+			wayX += move.steps
+			break
+		case W:
+			wayX -= move.steps
+			break
+		case R:
+			amount := move.steps / 90
+			dir = (dir + amount) % 4
+			wayX, wayY = rotateRight(amount, wayX, wayY)
+			break
+		case L:
+			amount := move.steps / 90
+			wayX, wayY = rotateRight(4 - amount, wayX, wayY)
+			dir = (dir - amount) % 4
+			if dir < 0 {
+				dir += 4
+			}
+			break
+		case F:
+			shipX += move.steps * wayX
+			shipY += move.steps * wayY
+			break
+		default:
+			log.Panic(fmt.Sprintf("Invalid direction dir: '%s'", move.dir))
+		}
+	}
+
+	shipX = absInt(shipX)
+	shipY = absInt(shipY)
+
+	return shipX + shipY
 }
